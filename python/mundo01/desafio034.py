5f94c7891975a048b56c4598f474fc79961395b9
Lucas Porfírio Marques <114091985+lporfirio@users.noreply.github.com>
2022-12-06 10:39:54 -0300

    atualizando

---
+++ python/mundo01/desafio034.py
@@ -0,0 +1,7 @@
+'''
+escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento.
+
+para salários superiores a 1250,00 calcule um aumento de 10%
+
+para os inferiores ou iguais, o aumento é de 15%
+'''
